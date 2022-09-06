0c076d240465fa4c3926b6940cd18e0be9543559
Kwarto <dbitsolu@gmail.com>
2022-09-06 04:39:23 +0000

    added swiper to the header

--- src/components/HeaderSlider/HeaderSLiderElement.jsx
+++ src/components/HeaderSlider/HeaderSLiderElement.jsx
@@ -15,24 +15,24 @@ export const HeaderSliderWrapper = styled.header`
  }
 `
 
-export const SliderMainWrapper = styled.div`
- height: 70vh;
- display: grid;
- grid-template-columns: 40% 60%;
- transform: scale(1.02);
-
- @media screen and (max-width: 1024px){
-  grid-template-columns: 100%;
-  height: 70vh;
- }
-`
-
 export const SliderItem = styled.div`
   padding: 30px;
   background: #fff;
   box-shadow: var(--box-shadow);
   display: flex;
   align-items: center;
+`
+ 
+export const SliderText = styled.h2`
+ color: #02010a;
+ font-size: 35px;
+ font-weight: 600;
+ text-align: center;
+ text-transform: uppercase;
+
+ @media screen and (max-width: 1024px){
+  font-size: 30px;
+ }
 `
 
 export const SliderImg = styled.div`
@@ -44,16 +44,5 @@ export const SliderImg = styled.div`
     &:hover{
         filter: brightness(1) saturate(1);
     }
- }
-`
-export const SliderText = styled.h2`
- color: #02010a;
- font-size: 35px;
- font-weight: 600;
- text-align: center;
- text-transform: uppercase;
-
- @media screen and (max-width: 1024px){
-  font-size: 30px;
- }
+ } 
 `
--- src/components/HeaderSlider/HeaderSlider.jsx
+++ src/components/HeaderSlider/HeaderSlider.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { HeaderSliderWrapper, SliderMainWrapper, SliderItem, SliderText, SliderImg } from './HeaderSLiderElement'
+import { HeaderSliderWrapper, SliderItem, SliderText, SliderImg } from './HeaderSLiderElement'
 import SlidImg1 from '../../images/nine.jpg'
+import SlidImg2 from '../../images/image-s4.png'
+import { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 function HeaderSlider() {
   return (
      <>
       <HeaderSliderWrapper>
-        <SliderMainWrapper className='container'>
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide className='swiper-slide'>
           <SliderItem>
             <SliderText>
               Helping you find right homes at right prices.
@@ -14,7 +28,28 @@ function HeaderSlider() {
           <SliderImg>
             <img src={SlidImg1} alt="" />
           </SliderImg>
-        </SliderMainWrapper>
+        </SwiperSlide>
+        <SwiperSlide className='container'>
+          <SliderItem>
+            <SliderText>
+              Helping you find right homes at right prices.
+            </SliderText>
+          </SliderItem>
+          <SliderImg>
+            <img src={SlidImg2} alt="" />
+          </SliderImg>
+        </SwiperSlide>
+        <SwiperSlide className='container'>
+          <SliderItem>
+            <SliderText>
+              Helping you find right homes at right prices.
+            </SliderText>
+          </SliderItem>
+          <SliderImg>
+            <img src={SlidImg1} alt="" />
+          </SliderImg>
+        </SwiperSlide>
+      </Swiper>
       </HeaderSliderWrapper>
      </>
   )
